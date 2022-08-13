f8d311c49ae301c6826a7febbaa63e33175c9487
Kyle Frederick <lilkyle98@gmail.com>
2022-08-13 10:55:12 -0400

    first commit

---
+++ src/components/Contact/Contact.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import { Navbar } from '../Navbar';
+
+export const Contact = () => {
+  return (
+    <div>
+        <Navbar />
+        Please do not contact me I am a hermit
+    </div>
+  )
+}
--- src/index.tsx
+++ src/index.tsx
@@ -1,19 +1,45 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import { FirebaseAppProvider, AuthCheck } from 'reactfire';
+import { Home, Contact, About, Phonebook, SignIn } from './components'
+import './style.css'
+import { firebaseConfig } from './firebaseConfig';
+import 'firebase/auth';
+import { Provider } from 'react-redux';
+import {store } from './redux/store';
+
+let myTitle = "Kyle's Phonebook"
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
   <React.StrictMode>
-    <App />
+  <FirebaseAppProvider firebaseConfig={firebaseConfig} suspense={true}>
+  <Provider store={store}>
+    <Router>
+      <Switch>
+
+        <Route exact path="/">
+          <Home title={myTitle} />
+        </Route>
+        <Route path='/phonebook'>
+          <Phonebook></Phonebook>
+        </Route>
+        <Route path='/contact'>
+          <Contact></Contact>
+        </Route>
+        <Route path='/about'>
+          <About></About>
+        </Route>
+        <Route path='/signin'>
+          <SignIn></SignIn>
+        </Route>
+
+      </Switch>
+    </Router>
+  </Provider>
+  </FirebaseAppProvider>
   </React.StrictMode>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
